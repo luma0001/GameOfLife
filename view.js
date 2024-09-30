8f6af1f25a40a2b95e54419d867aa8790a9bb156
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-09-30 11:05:12 +0200

    view don't import grid no longer

--- view.js
+++ view.js
@@ -16,13 +16,12 @@ function init() {
 
 function renderGrid(grid) {
   const board = document.querySelector("#board");
-  console.table(controller.grid);
   for (let row = 0; row < controller.GRID_HEIGHT; row++) {
     for (let col = 0; col < controller.GRID_WIDTH; col++) {
       const cell = board.querySelector(
         `[data-row='${row}'][data-col='${col}']`
       );
-      if (controller.grid.get(row, col) === 1) {
+      if (grid.get(row, col) === 1) {
         cell.style.backgroundColor = "black";
       } else {
         cell.style.backgroundColor = "white";
